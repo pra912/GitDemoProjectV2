9307e834583ab75370d508f8d331c525d2f1a63a
pra912 <pratikkumar972@gmail.com>
2021-05-30 13:52:11 +0530

    Code changes by Tester V2

--- src/EclipsetoGit_Test.java
+++ src/EclipsetoGit_Test.java
@@ -4,9 +4,9 @@ public class EclipsetoGit_Test {
 
 	public static void main(String[] args) {
 		
-		String[] names = {"Anushka" , "Pratik" , "Priyesh" , "Sneha"};
+		String[] names = {"Anushka" , "Pratik" , "Priyesh" , "Sneha" , "Vibha" , "Max"};
 		
-		System.out.println(names[new Random().nextInt(3)]);
+		System.out.println(names[new Random().nextInt(5)]);
 
 	}
 
