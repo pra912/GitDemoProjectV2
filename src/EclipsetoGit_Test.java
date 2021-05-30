fad266b9a6410fe2dca71aadd66832e808e30986
pra912 <pratikkumar972@gmail.com>
2021-05-30 13:17:46 +0530

    This is to commit version_2

---
+++ src/EclipsetoGit_Test.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class EclipsetoGit_Test {
+
+	public static void main(String[] args) {
+		
+		String[] names = {"Anushka" , "Pratik" , "Priyesh" , "Sneha"};
+		
+		System.out.println(names[new Random().nextInt(3)]);
+
+	}
+
+}
